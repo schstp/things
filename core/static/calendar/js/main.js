0c4fb1fd24806de22a0765f99f46beaecf7b5d05
Nikita <nikitakilpa@gmail.com>
2019-12-13 17:57:27 +0100

    Updating search function

--- core/static/calendar/js/main.js
+++ core/static/calendar/js/main.js
@@ -793,38 +793,51 @@ $(document).ready(function () {
         if (!searchMode)
         {
             searchMode = true;
+
             //hiding panel
             var hideElem = document.getElementById("hideElem");
             hideElem.style.display = "none";
             hideElem = document.getElementById("searchButton");
             hideElem.style.display = "none";
 
+            var searchPanel = document.getElementById("searchPanel");
+
+            //creating search box
+            var searchBox = document.createElement("div");
+            searchBox.id = "searchBox";
+            searchBox.className = "searchBox";
+
             //creating search field
-            var searchField = document.getElementById("searchField");
-            var searchItem = document.createElement("input");
-            searchItem.id = "searchItem";
-            searchItem.type = "text";
-            searchItem.classList = "searchBox";
+            var searchInput = document.createElement("input");
+            searchInput.id = "searchInput";
+            searchInput.type = "text";
+            searchInput.className = "searchInput";
+
+            //creating event list box
+            var eventBoxes = document.createElement("div");
+            eventBoxes.id = "eventBoxes";
+            eventBoxes.className = "eventBoxes";
 
             //creating cancel button
             var cancelButton = document.createElement("button");
             cancelButton.id = "cancelButton";
             cancelButton.className ="transp-button";
-            
             var cancelIcon = document.createElement("ion-icon");
-            cancelIcon.classList = "icons";
+            cancelIcon.className = "icons";
             cancelIcon.name = "ios-arrow-back";
             cancelButton.appendChild(cancelIcon);
 
             //creating cancel label
             var cancelLabel = document.createElement("label");
             cancelLabel.innerText = "Hide search bar...";
-            cancelLabel.classList = "cancelLabel";
+            cancelLabel.className = "cancelLabel";
 
             //appending elements
-            searchField.appendChild(cancelButton);
-            searchField.appendChild(cancelLabel);
-            searchField.appendChild(searchItem);
+            searchPanel.appendChild(cancelButton);
+            searchPanel.appendChild(cancelLabel);
+            searchBox.appendChild(searchInput);
+            searchBox.appendChild(eventBoxes);
+            searchPanel.appendChild(searchBox);
 
             // cancel button click event
             $('#cancelButton').on('click', function() 
@@ -832,10 +845,11 @@ $(document).ready(function () {
                 if (searchMode)
                 {
                     searchMode = false;
-                    //removing search field
-                    var searchField = document.getElementById("searchField");
-                    while (searchField.firstChild)
-                        searchField.removeChild(searchField.firstChild);
+
+                    //removing search panel
+                    var searchPanel = document.getElementById("searchPanel");
+                    while (searchPanel.firstChild)
+                        searchPanel.removeChild(searchPanel.firstChild);
 
                     //showing panel
                     var hideElem = document.getElementById("hideElem");
@@ -846,9 +860,9 @@ $(document).ready(function () {
             });
 
             //uploading event list
-            $('#searchItem').keyup(function (e) {
+            $('#searchInput').keyup(function (e) {
                 //check input
-                var searchStr = document.getElementById("searchItem").value;
+                var searchStr = document.getElementById("searchInput").value;
                 if (searchStr.length != 0)
                 {
                     $.ajax({
@@ -860,22 +874,64 @@ $(document).ready(function () {
                         },
                         dataType: 'json',
                         success: function (data) {
-                            for (let eventData of data.events) {
-                                showEvent(eventData.title, eventData.startDate, eventData.endDate);
-                            }
-                            console.log(data);
+                                var eventBoxes = document.getElementById("eventBoxes");
+                                
+                                while (eventBoxes.firstChild)
+                                    eventBoxes.removeChild(eventBoxes.firstChild);
+                                for (let eventData of data.events) {
+                                    showEvent(eventData.title, new Date(eventData.startDate), new Date(eventData.endDate));
+                                }
                         }
                     })
+                }
+                else
+                {
+                    var eventBoxes = document.getElementById("eventBoxes");
+                    while (eventBoxes.firstChild)
+                        eventBoxes.removeChild(eventBoxes.firstChild);
+                    eventBoxes.style.display = "none";
                 }
             });
 
             //showing uploaded events
-            $(function showEvent(title, startDate, endDate)
+            function showEvent(title, startDate, endDate)
             {
-                var searchField = document.getElementById("searchField");
+                var eventBoxes = document.getElementById("eventBoxes");
+                eventBoxes.style.display = "flex";
                 var eventBox = document.createElement("div");
-                //TO-DO
-            });
+                eventBox.id = "eventBox";
+                eventBox.className = "eventBox";
+
+                //title
+                var titleLabel = document.createElement("label");
+                titleLabel.innerText = title;
+                titleLabel.className = "eventBoxLeft";
+
+                //date and time
+                var timeLabel = document.createElement("label");
+                var dateTime = getWeekDay(startDate) + ", " + getTimeString(startDate.getHours()) + ":" + getTimeString(startDate.getMinutes()) + " - " 
+                        + getTimeString(endDate.getHours()) + ":" + getTimeString(endDate.getMinutes());
+                timeLabel.innerText = dateTime;
+                timeLabel.className = "eventBoxRight";
+
+                //appending elements
+                eventBox.appendChild(titleLabel);
+                eventBox.appendChild(timeLabel);
+                eventBoxes.appendChild(eventBox);
+            };
+
+            //transform time to 00:00 format
+            function getTimeString(time)
+            {
+                return time < 10 ? TIME_EDITS[time] : time;
+            }
+
+            //function return day of week
+            function getWeekDay(date) 
+            {
+                let days = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'];
+                return days[date.getDay()];
+            }
         }
     });
 });
